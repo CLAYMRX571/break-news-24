8386d9cdce4dc094b62bde3fa9695df764c34ce6
claymrx571 <claymrx571@gmail.com>
2024-09-10 16:51:32 +0500

    projects

--- news/contexts.py
+++ news/contexts.py
@@ -3,7 +3,7 @@ from .models import Category, Tag, Article
 def get_main_context(request):
     categories = Category.objects.all()
     tags = Tag.objects.all()
-    latest_news = Article.objects.all()[:12]
+    latest_news = Article.objects.filter()[:4]
 
     context = {
         "tags": tags,
