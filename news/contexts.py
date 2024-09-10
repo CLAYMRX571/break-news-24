477837f533e435d548517dae11763cbff7d5e435
claymrx571 <claymrx571@gmail.com>
2024-09-10 17:38:11 +0500

    new change

--- news/contexts.py
+++ news/contexts.py
@@ -1,12 +1,15 @@
 from .models import Category, Tag, Article
+from datetime import datetime
 
 def get_main_context(request):
     categories = Category.objects.all()
     tags = Tag.objects.all()
     latest_news = Article.objects.filter()[:4]
+    today_date = datetime.now().date()
 
     context = {
         "tags": tags,
+        "today_date": today_date,
         "categories": categories,
         "latest_news": latest_news,
     }
